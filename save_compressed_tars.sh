3fd8cd40e5d692df94de237eaca940899ec2c199
Michael Wang <schmickel@gmail.com>
2017-04-10 14:36:22 +1000

    Make pbzip2 most preferred option for compression

--- save_compressed_tars.sh
+++ save_compressed_tars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bz2app=bzip2
-if command -v pbzip2 > /dev/null 2>&1 ; then bz2app=pbzip2; fi
 if command -v lbzip2 > /dev/null 2>&1 ; then bz2app=lbzip2; fi
+if command -v pbzip2 > /dev/null 2>&1 ; then bz2app=pbzip2; fi
 
 version=17.03
 for image in caffe cntk digits mxnet tensorflow theano torch;
