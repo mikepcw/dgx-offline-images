e7aa34629cf5dd6c17a827d2cce2f8c844438f38
Michael Wang <schmickel@gmail.com>
2017-04-10 15:13:38 +1000

    Check md5sum of exploded tar (see note in readme)

---
+++ verify_checksums.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+bz2app=bzip2
+if command -v pbzip2 > /dev/null 2>&1 ; then bz2app=pbzip2; fi
+if command -v lbzip2 > /dev/null 2>&1 ; then bz2app=lbzip2; fi
+
+version=17.03
+for image in caffe cntk digits mxnet tensorflow theano torch;
+do
+    echo $image:$version
+    docker save nvcr.io/nvidia/$image:$version | tar xO | md5sum
+    tar --use-compress-prog=$bz2app -O -xf $image-$version.tar.bz2 | md5sum
+done
+
+cudatag=8.0-cudnn6-devel-ubuntu16.04
+echo cuda:$cudatag
+docker save nvcr.io/nvidia/cuda:$cudatag | tar xO | md5sum
+tar --use-compress-prog=$bz2app -O -xf cuda-$cudatag.tar.bz2 | md5sum
+
