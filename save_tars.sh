4b4e22a9288de8231822b75ee9cd8b427d5a6b78
Mike Wang <schmickel@gmail.com>
2017-06-04 12:51:38 +0000

    17.05 containers; added pytorch

--- save_tars.sh
+++ save_tars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-version=17.04
-for image in caffe caffe2 cntk digits mxnet tensorflow theano torch;
+version=17.05
+for image in caffe caffe2 cntk digits mxnet pytorch tensorflow theano torch;
 do
     docker pull nvcr.io/nvidia/$image:$version
     docker save nvcr.io/nvidia/$image:$version -o $image-$version.tar
