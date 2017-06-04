4b5897dfb36724c9cf92fe1bd1f20a1f97f89797
Mike Wang <schmickel@gmail.com>
2017-06-04 12:48:49 +0000

    17.05 containers

--- compress_tars.sh
+++ compress_tars.sh
@@ -3,7 +3,7 @@ bz2app=bzip2
 if command -v lbzip2 > /dev/null 2>&1 ; then bz2app=lbzip2; fi
 if command -v pbzip2 > /dev/null 2>&1 ; then bz2app=pbzip2; fi
 
-version=17.04
+version=17.05
 for image in *.tar;
 do
     $bz2app -vk $image
